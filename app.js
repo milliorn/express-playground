10fac716da40707252cca155973e7b757dcc4b41
milliorn <scottmilliorn@gmail.com>
2022-12-19 13:06:17 -0800

    Route paths

--- app.js
+++ app.js
@@ -7,6 +7,45 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// This route path will match requests to /about.
+app.get("/about", (req, res) => {
+  res.send("about");
+});
+
+// This route path will match requests to /random.text.
+app.get("/random.text", (req, res) => {
+  res.send("random.text");
+});
+
+// This route path will match acd and abcd.
+app.get("/ab?cd", (req, res) => {
+  res.send("ab?cd");
+});
+
+// This route path will match abcd, abbcd, abbbcd, and so on.
+app.get("/ab+cd", (req, res) => {
+  res.send("ab+cd");
+});
+
+// This route path will match abcd, abxcd, abRANDOMcd, ab123cd, and so on.
+app.get("/ab*cd", (req, res) => {
+  res.send("ab*cd");
+});
+
+// This route path will match /abe and /abcde.
+app.get("/ab(cd)?e", (req, res) => {
+  res.send("ab(cd)?e");
+});
+
+// This route path will match anything with an "z" in it.
+app.get(/z/, (req, res) => {
+  res.send("/z/");
+});
+
+// This route path will match butterfly and dragonfly, but not butterflyman, dragonflyman, and so on.
+app.get(/.*fly$/, (req, res) => {
+  res.send("./*fly$/");
+});
 // Respond to POST request on the root route (/), the application’s home page:
 app.post("/", (req, res) => {
   res.send("Received POST request");
