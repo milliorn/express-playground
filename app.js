af29b0c2b0b00599793ee3f2e3290c23061bbb65
milliorn <scottmilliorn@gmail.com>
2022-12-19 13:26:24 -0800

    Route handlers

--- app.js
+++ app.js
@@ -52,6 +52,52 @@ app.get("/user/:userId/books/:bookId", (req, res) => {
   res.send(req.params);
 });
 
+// A single callback function can handle a route.
+app.get("/example/a", (req, res) => {
+  res.send("Hello A!");
+});
+
+// More than one callback function can handle a route (make sure you specify the next object).
+app.get(
+  "/example/b",
+  (req, res, next) => {
+    console.log("Response will be sent by next()");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello from B!");
+  }
+);
+
+// An array of callback functions can handle a route.
+const callbackFirst = (req, res, next) => {
+  console.log("callback first");
+  next();
+};
+
+const callbackSecond = (req, res, next) => {
+  console.log("callback second");
+  next();
+};
+
+const callbackThird = (req, res, next) => {
+  console.log("callback third");
+  next();
+};
+
+// A combination of independent functions and arrays of functions can handle a route.
+app.get(
+  "/example/callback",
+  [callbackFirst, callbackSecond, callbackThird],
+  (req, res, next) => {
+    console.log("response sent by next function");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello callbacks!");
+  }
+);
+
 // Respond to POST request on the root route (/), the application’s home page:
 app.post("/", (req, res) => {
   res.send("Received POST request");
